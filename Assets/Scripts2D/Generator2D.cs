2d0e7780fe8454dfc7b8eeb266bce399a0e41486
AlexoAleksei <31552198+AlexoAleksei@users.noreply.github.com>
2021-02-17 22:02:01 +0300

    Add files via upload
    Updated Generator, Added Resize, Finished with PlaceRooms()

--- Assets/Scripts2D/Generator2D.cs
+++ Assets/Scripts2D/Generator2D.cs
@@ -30,14 +30,17 @@ using UnityEngine;
 using Random = System.Random;
 using Graphs;
 
-public class Generator2D : MonoBehaviour {
-    enum CellType {
+public class Generator2D : MonoBehaviour
+{
+    enum CellType
+    {
         None,
         Room,
         Hallway
     }
 
-    enum RoomType {
+    enum RoomType
+    {
         Empty,
         Entrance,
         Exit,
@@ -83,11 +86,13 @@ public class Generator2D : MonoBehaviour {
     }
 
     [SerializeField]
+    Vector2Int maxSize;
+    [SerializeField]
     Vector2Int size;
     [SerializeField]
     int maxRoomNum; //No less than 2
     [SerializeField]
-    float battleRoomRatio; //From (0.0f to 1.0f)
+    float battleRoomRatio; //From (0.0f to 1.0f), 0.4 recommended
     [SerializeField]
     int roomCount;
     [SerializeField]
@@ -107,13 +112,16 @@ public class Generator2D : MonoBehaviour {
     Delaunay2D delaunay;
     HashSet<Prim.Edge> selectedEdges;
 
-    void Start() {
+
+    void Start()
+    {
         Generate();
     }
 
-    void Generate() {
+    void Generate()
+    {
         random = new Random(0); //If empty - all random, if number - seed
-        grid = new Grid2D<CellType>(size, Vector2Int.zero);
+        grid = new Grid2D<CellType>(maxSize, Vector2Int.zero);
         rooms = new List<Room>();
 
         GenerateRooms();
@@ -134,105 +142,193 @@ public class Generator2D : MonoBehaviour {
         prevLevelInfo - влияет на скорость роста сложности с возрастанием 
                         номера уровня (сложность комнат, количество наград, сила врагов)
     }*/
-    void GenerateRooms() {
+    void GenerateRooms()
+    {
         int[] roomTypeNum = new int[Enum.GetNames(typeof(RoomType)).Length - 1]; // Don't need "Empty" rooms
         roomTypeNum[0] = 1; //entranceRoomNumber
         roomTypeNum[1] = 1; //exitRoomNumber
         roomTypeNum[2] = (int)((maxRoomNum - roomTypeNum[0] - roomTypeNum[1]) * battleRoomRatio); //battleRoomNumber
         roomTypeNum[3] = maxRoomNum - roomTypeNum[0] - roomTypeNum[1] - roomTypeNum[2]; //decorativeRoomNumber
 
-        Debug.Log(Enum.GetNames(typeof(RoomType)).Length);
-        Debug.Log(roomTypeNum[0]);
+        //Debug.Log(Enum.GetNames(typeof(RoomType)).Length);
+        /*Debug.Log(roomTypeNum[0]);
         Debug.Log(roomTypeNum[1]);
         Debug.Log(roomTypeNum[2]);
         Debug.Log(roomTypeNum[3]);
+        Debug.Log(roomTypeNum.Length);*/
 
-       for (int i = 0; i < roomTypeNum.Length; i++) {
-            RoomType type = RoomType.Empty;
+        for (int i = 0; i < roomTypeNum.Length; i++)
+        {
+            for (int j = 0; j < roomTypeNum[i]; j++)
+            {
+                RoomType type = RoomType.Empty;
+                Vector2Int location = new Vector2Int(-2, -2);
+                Vector2Int roomSize = new Vector2Int(0, 0);
 
-            switch (i) {
-                case 0:
-                    type = RoomType.Entrance;
-                    break;
-                case 1:
-                    type = RoomType.Exit;
-                    break;
-                case 2:
-                    type = RoomType.Battle;
-                    break;
-                case 3:
-                    type = RoomType.Decorative;
-                    break;
-                default:
-                    type = RoomType.Empty;
-                    break;
-            }
-
-            Vector2Int location = new Vector2Int(0, 0);
-
-            Vector2Int roomSize = new Vector2Int(
-                random.Next(roomMinSize.x, roomMaxSize.x),
-                random.Next(roomMinSize.y, roomMaxSize.y)
-            );
-
-            Room newRoom = new Room(location, roomSize, type);
-            rooms.Add(newRoom);
-        }
-    }
-
-    void PlaceRooms() {
-        for (int i = 0; i < roomCount; i++) {
-            Vector2Int location = new Vector2Int(
-                random.Next(0, size.x),
-                random.Next(0, size.y)
-            );
-
-            Vector2Int roomSize = new Vector2Int(
-                random.Next(1, roomMaxSize.x + 1),
-                random.Next(1, roomMaxSize.y + 1)
-            );
-
-            bool add = true;
-            Room newRoom = new Room(location, roomSize);
-            Room buffer = new Room(location + new Vector2Int(-1, -1), roomSize + new Vector2Int(2, 2));
-
-            foreach (var room in rooms) {
-                if (Room.Intersect(room, buffer)) {
-                    add = false;
-                    break;
+                switch (i)
+                {
+                    case 0:
+                        type = RoomType.Entrance;
+                        break;
+                    case 1:
+                        type = RoomType.Exit;
+                        break;
+                    case 2:
+                        type = RoomType.Battle;
+                        break;
+                    case 3:
+                        type = RoomType.Decorative;
+                        break;
+                    default:
+                        type = RoomType.Empty;
+                        break;
                 }
-            }
-
-            if (newRoom.bounds.xMin < 0 || newRoom.bounds.xMax >= size.x
-                || newRoom.bounds.yMin < 0 || newRoom.bounds.yMax >= size.y) {
-                add = false;
-            }
-
-            if (add) {
+                Room newRoom = new Room(location, roomSize, type);
                 rooms.Add(newRoom);
-                PlaceRoom(newRoom.bounds.position, newRoom.bounds.size);
 
-                foreach (var pos in newRoom.bounds.allPositionsWithin) {
-                    grid[pos] = CellType.Room;
-                }
+                /*Debug.Log("index =");
+                Debug.Log(i);
+                Debug.Log("Type =");
+                Debug.Log(rooms[i].Type);*/
             }
         }
     }
 
-    void Triangulate() {
+    void PlaceRooms()
+    {
+        List<Room> roomsCopy = new List<Room>(); //Creating a reserve copy
+        roomsCopy.AddRange(rooms);
+        
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            for (int j = 0; j < roomCount; j++)
+            {
+                rooms[i].bounds.position = new Vector2Int(
+                    random.Next(0, size.x),
+                    random.Next(0, size.y)
+                );
+
+                switch (rooms[i].Type)
+                {
+                    case RoomType.Entrance:
+                        rooms[i].bounds.size = new Vector2Int(2, 2);
+                        break;
+                    case RoomType.Exit:
+                        rooms[i].bounds.size = new Vector2Int(2, 2);
+                        break;
+                    case RoomType.Battle:
+                        int x = random.Next(roomMinSize.x + 1, roomMaxSize.x);
+                        int y = random.Next(x - 1, x + 1);
+                        rooms[i].bounds.size = new Vector2Int(x, y);
+                        break;
+                    case RoomType.Decorative:
+                        x = random.Next(roomMinSize.x + 1, roomMaxSize.x - 1);
+                        y = random.Next(x - 1, x + 1);
+                        rooms[i].bounds.size = new Vector2Int(x, y);
+                        break;
+                    default:
+                        rooms[i].bounds.size = new Vector2Int(roomMinSize.x, roomMinSize.y);
+                        break;
+                }
+
+                /*Debug.Log(i);
+                Debug.Log(rooms[i].bounds.position);
+                Debug.Log(rooms[i].bounds.size);*/
+
+                bool add = true;
+                Room buffer = new Room(
+                    rooms[i].bounds.position + new Vector2Int(-1, -1),
+                    rooms[i].bounds.size + new Vector2Int(2, 2)
+                );
+
+                foreach (var room in rooms)
+                {
+                    if (Room.Intersect(room, buffer) && room != rooms[i])
+                    {
+                        //Debug.Log("Intersection!");
+                        add = false;
+                        break;
+                    }
+                }
+
+                if (rooms[i].bounds.xMin < 0 || rooms[i].bounds.xMax >= size.x
+                    || rooms[i].bounds.yMin < 0 || rooms[i].bounds.yMax >= size.y)
+                {
+                    //Debug.Log("OverField!");
+                    add = false;
+                }
+
+                if (add)
+                {                   
+                    break;
+                }
+
+                if ((j == roomCount - 1) && !add) //Здесь комментарии не нужно удалять
+                {
+                    if (rooms[i].Type == RoomType.Entrance || rooms[i].Type == RoomType.Exit || rooms.Count <= 3)
+                    {
+                        Debug.Log("Retrying to place rooms!");
+                        rooms.Clear();
+                        rooms.AddRange(roomsCopy);
+                        ResizeField();
+                        i = -1;
+                        break;
+                    }                   
+                    else
+                    {
+                        Debug.Log("Removing at");
+                        Debug.Log(i);
+                        Debug.Log("Type = ");
+                        Debug.Log(rooms[i].Type);
+                        rooms.RemoveAt(i);
+                        i -= 1;
+                    }
+                }
+            }
+        }
+
+        foreach (var room in rooms)
+        {
+            PlaceRoom(room.bounds.position, room.bounds.size); //Models
+
+            foreach (var pos in room.bounds.allPositionsWithin) //Grid positions
+            {
+                grid[pos] = CellType.Room;
+            }
+        }
+
+        /*if (rooms.Count < 3) //Triangulation can't work with less than 3 rooms
+        {
+            Room newRoom = new Room(new Vector2Int(-1, -1), new Vector2Int(1, 1), RoomType.Empty);
+            rooms.Add(newRoom);
+        }*/
+
+        Debug.Log("Rooms number = ");
+        Debug.Log(rooms.Count);
+        foreach (var room in rooms)
+        {
+            Debug.Log(room.Type);
+        }
+    }
+
+    void Triangulate()
+    {
         List<Vertex> vertices = new List<Vertex>();
 
-        foreach (var room in rooms) {
+        foreach (var room in rooms)
+        {
             vertices.Add(new Vertex<Room>((Vector2)room.bounds.position + ((Vector2)room.bounds.size) / 2, room));
         }
 
         delaunay = Delaunay2D.Triangulate(vertices);
     }
 
-    void CreateHallways() {
+    void CreateHallways()
+    {
         List<Prim.Edge> edges = new List<Prim.Edge>();
 
-        foreach (var edge in delaunay.Edges) {
+        foreach (var edge in delaunay.Edges)
+        {
             edges.Add(new Prim.Edge(edge.U, edge.V));
         }
 
@@ -242,17 +338,21 @@ public class Generator2D : MonoBehaviour {
         var remainingEdges = new HashSet<Prim.Edge>(edges);
         remainingEdges.ExceptWith(selectedEdges);
 
-        foreach (var edge in remainingEdges) {
-            if (random.NextDouble() < 0.125) {
+        foreach (var edge in remainingEdges)
+        {
+            if (random.NextDouble() < 0.125)
+            {
                 selectedEdges.Add(edge);
             }
         }
     }
 
-    void PathfindHallways() {
+    void PathfindHallways()
+    {
         DungeonPathfinder2D aStar = new DungeonPathfinder2D(size);
 
-        foreach (var edge in selectedEdges) {
+        foreach (var edge in selectedEdges)
+        {
             var startRoom = (edge.U as Vertex<Room>).Item;
             var endRoom = (edge.V as Vertex<Room>).Item;
 
@@ -263,14 +363,19 @@ public class Generator2D : MonoBehaviour {
 
             var path = aStar.FindPath(startPos, endPos, (DungeonPathfinder2D.Node a, DungeonPathfinder2D.Node b) => {
                 var pathCost = new DungeonPathfinder2D.PathCost();
-                
+
                 pathCost.cost = Vector2Int.Distance(b.Position, endPos);    //heuristic
 
-                if (grid[b.Position] == CellType.Room) {
+                if (grid[b.Position] == CellType.Room)
+                {
                     pathCost.cost += 10;
-                } else if (grid[b.Position] == CellType.None) {
+                }
+                else if (grid[b.Position] == CellType.None)
+                {
                     pathCost.cost += 5;
-                } else if (grid[b.Position] == CellType.Hallway) {
+                }
+                else if (grid[b.Position] == CellType.Hallway)
+                {
                     pathCost.cost += 1;
                 }
 
@@ -279,23 +384,29 @@ public class Generator2D : MonoBehaviour {
                 return pathCost;
             });
 
-            if (path != null) {
-                for (int i = 0; i < path.Count; i++) {
+            if (path != null)
+            {
+                for (int i = 0; i < path.Count; i++)
+                {
                     var current = path[i];
 
-                    if (grid[current] == CellType.None) {
+                    if (grid[current] == CellType.None)
+                    {
                         grid[current] = CellType.Hallway;
                     }
 
-                    if (i > 0) {
+                    if (i > 0)
+                    {
                         var prev = path[i - 1];
 
                         var delta = current - prev;
                     }
                 }
 
-                foreach (var pos in path) {
-                    if (grid[pos] == CellType.Hallway) {
+                foreach (var pos in path)
+                {
+                    if (grid[pos] == CellType.Hallway)
+                    {
                         PlaceHallway(pos);
                     }
                 }
@@ -303,17 +414,57 @@ public class Generator2D : MonoBehaviour {
         }
     }
 
-    void PlaceCube(Vector2Int location, Vector2Int size, Material material) {
+    void ResizeField() //Increases the size of the field used, max is (maxSize.x, maxSize.y)
+    {
+        if (size.x >= maxSize.x || size.y >= maxSize.y)
+        {
+            Debug.Log("The max field size is exceeded!");
+        }
+
+        size.x += (int)(size.x * 0.1f) + 1;
+        size.y += (int)(size.y * 0.1f) + 1;
+
+        if (size.x >= maxSize.x)
+        {
+            size.x = maxSize.x;
+        }
+        if (size.y >= maxSize.y)
+        {
+            size.y = maxSize.y;
+        }
+    }
+
+    /*void ClearField() //Clears the filed within ths size.x and size.y
+    {
+        Vector2Int pos = new Vector2Int(0, 0);
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                pos = new Vector2Int(x, y);
+                Debug.Log(pos);
+                Debug.Log(grid[pos]);
+                grid[pos] = CellType.None;
+                Debug.Log(grid[pos]);
+            }          
+        }
+        Debug.Log("Cleared!");
+    }*/
+
+    void PlaceCube(Vector2Int location, Vector2Int size, Material material)
+    {
         GameObject go = Instantiate(cubePrefab, new Vector3(location.x, 0, location.y), Quaternion.identity);
         go.GetComponent<Transform>().localScale = new Vector3(size.x, 1, size.y);
         go.GetComponent<MeshRenderer>().material = material;
     }
 
-    void PlaceRoom(Vector2Int location, Vector2Int size) {
+    void PlaceRoom(Vector2Int location, Vector2Int size)
+    {
         PlaceCube(location, size, redMaterial);
     }
 
-    void PlaceHallway(Vector2Int location) {
+    void PlaceHallway(Vector2Int location)
+    {
         PlaceCube(location, new Vector2Int(1, 1), blueMaterial);
     }
 }
